7099c13023b2f426f916ea78150f0768190e30b9
Rakesh verma <rakesh.verma0487@gmail.com>
2014-01-11 18:01:18 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffeeBean::Application.config.secret_key_base = 'be975438ca0f89345ddf81ec186b521e19ff65dfc74918935791609f2155b8d7d768369cf47137942b768203a1998d97842fe7c3d16131395f3c4b583978b1f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoffeeBean::Application.config.session_store :cookie_store, key: '_coffee-bean_session'
